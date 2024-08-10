7093f296ed8765b94084c546581907a396c6873a
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-10 15:13:53 +0545

    sdv

--- preconstruction/admin.py
+++ preconstruction/admin.py
@@ -13,7 +13,7 @@ class PreConstructionFloorPlanAdmin(admin.StackedInline):
 @admin.register(PreConstruction)
 class PreConstructionAdmin(admin.ModelAdmin):
     list_display = ("project_name", "city", "project_type")
-    search_fields = ("project_name")
+    search_fields = ("project_name","project_type")
 
     class Meta:
         model = PreConstruction
