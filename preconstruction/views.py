6175c66b6c4f82ba91b849bc9cd88c494badc2f4
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-11 15:59:19 +0545

    DFV

--- preconstruction/views.py
+++ preconstruction/views.py
@@ -48,7 +48,7 @@ def robotView(request):
  """
 
 class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
