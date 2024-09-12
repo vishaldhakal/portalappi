83706f5963981f54d0bc0e9bd36ef8cbec85aef6
vishaldhakal <vishaldhakal96@gmail.com>
2024-09-12 13:25:42 +0545

    Dfv

--- widget_tracker/models.py
+++ widget_tracker/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
-    idd = models.CharField(max_length=50, primary_key=True)
+    idd = models.IntegerField(default=1, primary_key=True)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
