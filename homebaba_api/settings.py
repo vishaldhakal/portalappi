c8f746857ef1995b1add4dbad1d2244db5353661
Naseeb <40313494+naseeb0@users.noreply.github.com>
2024-10-22 15:28:15 +0545

    server db bisechu haha

--- homebaba_api/settings.py
+++ homebaba_api/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'homebaba_api.wsgi.application'
 DATA_UPLOAD_MAX_MEMORY_SIZE = None
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} 
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'condomonk',
-#         'USER': 'condomonk_admin',
-#         'PASSWORD': 'condomonkadminpassword',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+# } 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'condomonk',
+        'USER': 'condomonk_admin',
+        'PASSWORD': 'condomonkadminpassword',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
